e5a14f98c6e2ecb1b577f4f320db8413c9846b77
bayedamediop <bayedame1823@gmail.com>
2021-02-25 09:13:43 +0000

    deminuer la somme

--- migrations/Version20210225085046.php
+++ migrations/Version20210225085046.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210223140054 extends AbstractMigration
+final class Version20210225085046 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,12 +20,12 @@ final class Version20210223140054 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user DROP roles');
+        $this->addSql('CREATE TABLE agences (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user ADD roles JSON NOT NULL');
+        $this->addSql('DROP TABLE agences');
     }
 }
